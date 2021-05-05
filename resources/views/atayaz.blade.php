697de371539195702a786adaa8a623cb08d99890
BRKFun <dzeko>
2021-05-05 20:36:15 +0300

    last fix

--- resources/views/atayaz.blade.php
+++ resources/views/atayaz.blade.php
@@ -4,19 +4,11 @@
     <div class="container">
         <header>
             <div style="text-align:center"><p>
+                </p>
+            </div>
         </header>
-        <div class ="tableouter">
-            <form action="işlem.php" method="post">
-                <div style="text-align:center"><p>
-                        <a href="{{ route('loginPage') }}">
-                            Müşteri Giriş
-                        </a>
-                        <input type="button" class="sub" id="müşteri" onClick="" value="müşteri giriş" />
-                        <input type="button" class="sub" id="personel" onClick="location.href='personel.php'" value="personel giriş" />
-                    <form action="login.php" method="POST">
-                        <br>
-                    </form>
-                </div>
-                <img src="https://r.resimlink.com/5hKMF9R.png"width="1850" height="950" >
+        <div class="tableouter">
+        </div>
     </div>
+
 @endsection
