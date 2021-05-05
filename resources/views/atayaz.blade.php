17f38c8d0c1c2c3bd51118b4c5180dbf06ab4083
BRKFun <dzeko>
2021-05-05 19:22:54 +0300

    test + auth

--- resources/views/atayaz.blade.php
+++ resources/views/atayaz.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/css/style.css">
     <title>sinema gişesi</title>
 </head>
 <body>
@@ -19,8 +19,11 @@
     <div class ="tableouter"> 
     <form action="işlem.php" method="post">
     <div style="text-align:center"><p>
-    <input type="button" class="sub" id="müşteri" onClick="location.href='müşteri.php'" value="müşteri giriş" />
-    <input type="button" class="sub" id="personel" onClick="location.href='personel.php'" value="personel giriş" />
+            <a href="">
+                Müşteri Giriş
+            </a>
+            <input type="button" class="sub" id="müşteri" onClick="" value="müşteri giriş" />
+            <input type="button" class="sub" id="personel" onClick="location.href='personel.php'" value="personel giriş" />
     <form action="login.php" method="POST">
     <br>
      </form>
--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,7 @@ Route::get('/atayaz', function () {
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
